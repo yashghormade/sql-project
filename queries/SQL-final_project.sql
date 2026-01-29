0d0a6ac1204970d9c93228b20f125819b1113433
Yash Ghormade <161242093+yashghormade@users.noreply.github.com>
2026-01-29 09:34:17 +0530

    Create  SQL-final_project.sql
    Added SQL e-commerce analysis project
    Contains SQL queries for analyzing customers, orders, products, payments, and revenue in an e-commerce database.

---
+++ queries/SQL-final_project.sql
@@ -0,0 +1,49 @@
+1- SQL E-Commerce Analysis Project
+
+2-📌 Project Overview
+This project performs end-to-end SQL analysis on an e-commerce database to extract meaningful business insights related to customers, orders, products, payments, and revenue.
+
+3- 🗂 Database Tables
+- customers
+- orders
+- order_items
+- payments
+- products
+
+4- 🛠 Tools & Technologies
+- MySQL
+- SQL
+- GitHub
+
+5- 🔍 SQL Concepts Used
+- Joins (INNER, LEFT)
+- Subqueries & Correlated Subqueries
+- Aggregations (SUM, COUNT, AVG)
+- GROUP BY & HAVING
+- Stored Procedures
+- Analytical Queries
+
+6- 📊 Business Questions Solved
+- Customers with more than 3 orders
+- Top 5 customers by total spending
+- Products that were never ordered
+- Revenue analysis by product category
+- Failed payment but delivered orders
+- High-value customers above average spending
+
+7- 📁 Project Structure
+sql-project/
+│
+├── README.md
+└── queries/
+    └── final_project.sql
+
+9-👤 Author
+Yash Ghormade
+
+
+10- 🖼 Query Outputs
+Screenshots of query results are available in the screenshots folder.
+
+11- ⚡ Performance Optimization
+Indexes were considered on frequently joined columns like customer_id and order_id.
